27f36f70a191a82eaa951f47861074837ced552e
Peter Milne <peter.milne@d-tacq.com>
2020-03-16 20:42:18 +0000

    tailz is correct all values 	modified:   cpsc2_load_awg.py

--- cpsc2_load_awg.py
+++ cpsc2_load_awg.py
@@ -69,6 +69,7 @@ def run_main():
     parser.add_argument('--mask', default='(1,-1,0.9,-0.9,0.8,-0.8,0.1,-0.1)', help="channel scale factors")
     parser.add_argument('--txsfp', default=0, type=int, help="transmit to sfp mask 1:A, 2:B, 3:both")
     parser.add_argument('--fun', default='np.sin', type=str, help="function np.sin, np.sinh, np.cos etc")
+    parser.add_argument('--tailz', default=1, type=int, help="trailing zero value count [1]")
     parser.add_argument('uut', nargs=1, help="uut")
     args = parser.parse_args()
     args.mask = eval(args.mask)
@@ -77,7 +78,7 @@ def run_main():
 #    args.wf=args.amplitude*np.sin(2*np.pi*tt/args.nsam)
     fx = eval(args.fun)
     args.wf=args.amplitude*fx(2*np.pi*tt/args.nsam)
-    args.wf[args.nsam-1] = 0
+    args.wf[args.nsam-(args.tailz+1):] = 0
 
     AwgController(args)
 
